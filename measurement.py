678236f2db79d8fa190c108743e02212e52772fa
kelvin27315 <seven_units@yahoo.co.jp>
2020-09-19 03:06:31 +0900

    リファクタリング&グラフ表示の改良

--- measurement.py
+++ measurement.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-"""
-温度を表示する
-"""
-
 from pathlib import Path
 from board import D17
 import matplotlib.pyplot as plt
@@ -11,60 +7,67 @@ import datetime as dt
 import pandas as pd
 import adafruit_dht, os, time
 
-def measurement():
-    dht_device = adafruit_dht.DHT22(D17)
-    temperature = dht_device.temperature
-    humidity = dht_device.humidity
-    #print("湿度:\t{:.1f}%".format(humidity))
-    #print("温度:\t{:.1f}℃".format(temperature))
-    tz = dt.timezone(dt.timedelta(hours=+9), "Asia/Tokyo")
-    time = dt.datetime.now(tz).time()
+class Measurement():
+    def __init__(self):
+        self.path = Path(__file__).parent.resolve()
+        self.today = dt.date.today()
+        self.yesterday = self.today - dt.timedelta(days=1)
 
-    file_name = "{}.csv".format(dt.date.today())
-    file_name = Path(__file__).parent.resolve() / "data" / file_name
+    def get_csv_file_name(self, day):
+        return(self.path / "data" / "{}.csv".format(day))
 
-    data = "\n{},{},{}".format(time.hour*60+time.minute, humidity, temperature)
-    if os.path.isfile(file_name):
-	    with open(file_name, "a") as f:
-	        f.write(data)
-    else:
-	    with open(file_name, "w") as f:
-	        f.write(",humidity,temperature{}".format(data))
+    def measurement(self):
+        d = adafruit_dht.DHT22(D17)
+        file_name = self.get_csv_file_name(self.today())
 
-def make_graph():
-    file_name = "{}.csv".format(dt.date.today())
-    file_name = Path(__file__).parent.resolve() / "data" / file_name
-    df = pd.read_csv(file_name, index_col=0)
-    fig, ax = plt.subplots(2,1)
-    fig.suptitle("Temperature and Humidity on the {}".format(dt.date.today()))
-    ax[0].plot(df["temperature"], label="Temperature")
-    ax[0].set_title("Temperature")
-    ax[0].set_ylabel("temperature")
-    ax[0].legend()
-    ax[0].grid(True)
-    #ax[0].set_yticklabels(["{}C".format(int(t)) if t.is_integer() else "{}C".format(t) for t in plt.yticks()[0]])
+        t = dt.datetime.now(dt.timezone(dt.timedelta(hours=+9), "Asia/Tokyo")).time()
+        data = "\n{},{},{}".format(t.hour*60+t.minute, d.humidity, d.temperature)
+        if not os.path.isfile(file_name):
+            data = ",humidity,temperature" + data
 
-    ax[1].plot(df["humidity"], label="Humidity")
-    ax[1].set_title("Humidity")
-    ax[1].set_xlabel("time")
-    ax[1].set_ylabel("humidity")
-    ax[1].legend()
-    ax[1].grid(True)
-    label = ["{}:00".format(int(t/60)) if t%60 == 0 else "{}:{}".format(int(t/60),int(t%60)) for t in plt.xticks()[0]]
-    ax[0].set_xticks(plt.xticks()[0])
-    ax[0].set_xticklabels(label)
-    ax[1].set_xticks(plt.xticks()[0])
-    ax[1].set_xticklabels(label)
-    ax[1].set_yticks(plt.yticks()[0])
-    ax[1].set_yticklabels(["{}%".format(int(t)) if t.is_integer() else "{}%".format(t) for t in plt.yticks()[0]])
-    fig.tight_layout()
-    file_name = Path(__file__).parent.resolve() / "graph.png"
-    plt.savefig(file_name)
-    plt.close()
+        with open(file_name, "a") as f:
+            f.write(data)
+
+    def rename_xticklabes(self, old_label):
+        new_label = [""] * len(old_label)
+        for i,time in enumerate(old_label):
+            if time < 1440:
+                day = "{}-{}".format(self.yesterday.month, self.yesterday.day)
+            else:
+                day = "{}-{}".format(self.today.month, self.today.day)
+                time -= 1440
+            new_label[i] = "{}\n{}:00".format(day, int(time/60)) if time%60 == 0 else "{}\n{}:{}".format(day, int(time/60),int(time%60))
+        return(new_label)
+
+    def make_graph(self):
+        df = pd.concat([pd.read_csv(self.get_csv_file_name(self.yesterday), index_col=0),
+                        pd.read_csv(self.get_csv_file_name(self.today), index_col=0).rename(index=lambda i: i+1440)])
+        fig, axs = plt.subplots(2,1, figsize=(7,6))
+        fig.suptitle("Temperature and Humidity on the {}".format(self.today))
+        last_time = df.index.values[-1]
+        for ax, title in zip(axs, ("Temperature", "Humidity")):
+            ax.plot(df[title.lower()], label=title)
+            ax.set_title(title)
+            ax.set_ylabel(title)
+            ax.set_xlabel("time")
+            ax.set_xlim(last_time - 1440, last_time)
+            ax.legend()
+            ax.grid(True)
+            ax.set_xticks(ax.get_xticks())
+            ax.set_yticks(ax.get_yticks())
+            ax.set_xticklabels(self.rename_xticklabes(ax.get_xticks()))
+
+        axs[0].set_yticklabels(["{}C".format(int(t)) if t.is_integer() else "{}C".format(t) for t in axs[0].get_yticks()])
+        axs[1].set_yticklabels(["{}%".format(int(t)) if t.is_integer() else "{}%".format(t) for t in axs[1].get_yticks()])
+        fig.tight_layout()
+        file_name = Path(__file__).parent.resolve() / "graph.png"
+        plt.savefig(file_name)
+        plt.close()
 
 def main():
-    measurement()
-    make_graph()
+    m = Measurement()
+    m.measurement()
+    m.make_graph()
 
 if __name__ == "__main__":
     main()
